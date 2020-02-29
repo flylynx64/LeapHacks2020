d325d70489995292754b7226c683694b24a61b2b
Zachary Wang <47799146+flylynx64@users.noreply.github.com>
2020-02-29 15:35:58 -0600

    Create DatePickerFragment.java

---
+++ DatePickerFragment.java
@@ -0,0 +1,19 @@
+package com.example.leaphacks; //change this
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import java.util.Calendar;
+import androidx.fragment.app.DialogFragment;
+
+public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+    }
+}
