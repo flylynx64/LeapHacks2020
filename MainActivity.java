8a93a668d93fafc904928699950f65a7975a787a
Kevin Wu <47039448+rainy-blue@users.noreply.github.com>
2020-02-29 18:36:55 -0600

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,42 +1,28 @@
-package com.example.leaphacks; //change this 
+package com.example.newtest;
 
-import android.app.DatePickerDialog;
-import java.util.Calendar;
-import androidx.fragment.app.DialogFragment;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import java.text.DateFormat;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+import android.view.View;
+import android.widget.Button;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DialogFragment datePicker = new DatePickerFragment();
-        datePicker.show(getSupportFragmentManager(), "date picker");
-    }
 
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.YEAR, year);
-        c.set(Calendar.MONTH, month);
-        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
-
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(currentDateString);
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button add_button = (Button) findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                DialogFragment datePicker = new DatePickerFragment();
-                datePicker.show(getSupportFragmentManager(), "date picker");
+            public void onClick(View view){
+                
             }
+
         });
+
     }
 }
